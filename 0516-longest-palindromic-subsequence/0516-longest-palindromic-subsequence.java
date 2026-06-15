7d29aa693c0b70e6e0757d5ff8a9ff5786208d80
palle abhinay <palleabhinay462@gmail.com>
2026-06-15 22:09:59 +0530

    Time: 59 ms (26.1%), Space: 63.4 MB (82.02%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,26 +1,27 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        // Reverse the original string
-        String rev = new StringBuilder(s).reverse().toString();
-        
-        char[] str1 = s.toCharArray();
-        char[] str2 = rev.toCharArray();
-        int n = str1.length;
-
-        // DP table for LCS
-        int[][] dp = new int[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    // Characters match
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    // Mismatch: take max of excluding one char from either string
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        String r = new StringBuilder(s).reverse().toString();
+        int n=s.length();
+        return helper(s,r,n);
+    }
+    public int helper(String s,String r,int n){
+        int[][] dp=new int[n+1][n+1];
+        for(int i=0;i<n;i++){
+            dp[0][i]=0;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==r.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+    //    System.out.println(Arrays.deepToString(dp));
         return dp[n][n];
     }
 }
