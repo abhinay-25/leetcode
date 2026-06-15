81fddfdd3f09ed2702c2831d4bb214d683322750
palle abhinay <palleabhinay462@gmail.com>
2026-06-15 22:57:15 +0530

    Time: 26 ms (58.93%), Space: 47.5 MB (49.99%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,36 +1,31 @@
 class Solution {
     public int minInsertions(String s) {
-
-        String t = new StringBuilder(s).reverse().toString();
-
-        int lps = longestCommonSubsequence(s, t);
-
-        return s.length() - lps;
+         return s.length()-longestPalindromeSubseq(s);
+         
     }
-
-    private int longestCommonSubsequence(String text1, String text2) {
-
-        int n = text1.length();
-        int m = text2.length();
-
-        int[] prev = new int[m + 1];
-
-        for (int i = 1; i <= n; i++) {
-
-            int[] cur = new int[m + 1];
-
-            for (int j = 1; j <= m; j++) {
-
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    cur[j] = 1 + prev[j - 1];
-                } else {
-                    cur[j] = Math.max(prev[j], cur[j - 1]);
+    public int longestPalindromeSubseq(String s) {
+        String r = new StringBuilder(s).reverse().toString();
+        int n=s.length();
+        return helper(s,r,n);
+    }
+    public int helper(String s,String r,int n){
+        int[][] dp=new int[n+1][n+1];
+        for(int i=0;i<n;i++){
+            dp[0][i]=0;
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==r.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-
-            prev = cur;
         }
-
-        return prev[m];
+    //    System.out.println(Arrays.deepToString(dp));
+        return dp[n][n];
     }
 }
