af5a6616f74d7cc04bfbd3a6674a08fbd91c9253
palle abhinay <palleabhinay462@gmail.com>
2026-06-16 23:24:50 +0530

    Time: 535 ms (25.81%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,5 @@
-SELECT today.id
-FROM Weather yesterday 
-CROSS JOIN Weather today
-
-WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
-    AND today.temperature > yesterday.temperature
-;
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather AS w1
+INNER JOIN Weather AS w2
+WHERE DATEDIFF(w1.recordDate,w2.recordDate)=1 AND w1.temperature >w2.temperature;
