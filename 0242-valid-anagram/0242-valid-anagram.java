0e4b7afcead53c64635db38b0d188f5f7997fc4f
palle abhinay <palleabhinay462@gmail.com>
2026-06-16 15:01:20 +0530

    Time: 16 ms (21.45%), Space: 46.3 MB (40.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -13,15 +13,8 @@ class Solution {
             char ch=t.charAt(i);
             mapT.put(ch,mapT.getOrDefault(ch,0)+1);
         }
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(!mapS.get(ch).equals(mapT.get(ch))){
-                return false;
-            }else{
-                continue;
-            }
-        }
         
-        return true;
+        
+        return mapS.equals(mapT);
     }
 }
