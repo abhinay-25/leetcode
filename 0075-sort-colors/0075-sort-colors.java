32266eec9db9a327e3f8aaf94dd352873cbda5fa
palle abhinay <palleabhinay462@gmail.com>
2026-06-21 12:49:48 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int freq[]= new int[3];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                freq[0]++;
+            }else if(nums[i]==1){
+                freq[1]++;
+            }else{
+                freq[2]++;
+            }
+        }
+        int k=0;
+        for(int i=0;i<freq[0];i++){
+            nums[k++]=0;
+        }
+        for(int i=0;i<freq[1];i++){
+            nums[k++]=1;
+        }
+        for(int i=0;i<freq[2];i++){
+            nums[k++]=2;
+        }
     }
 }
